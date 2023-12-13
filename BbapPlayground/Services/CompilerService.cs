21ca0b3013ee9da310bc50206d139d62fbcd83a5
yannikHoeflich <ygidyyy@gmail.com>
2023-12-13 17:26:06 +0100

    Added: compile timer

--- BbapPlayground/Services/CompilerService.cs
+++ BbapPlayground/Services/CompilerService.cs
@@ -1,4 +1,5 @@
-﻿using BBAP;
+﻿using System.Diagnostics;
+using BBAP;
 using BBAP.Config;
 using BBAP.Results;
 using Microsoft.AspNetCore.Components;
@@ -6,14 +7,16 @@ using Microsoft.AspNetCore.Components;
 namespace BbapPlayground.Services;
 
 public class CompilerService {
-    public MarkupString Compile(string code) {
+    public (MarkupString markupString, TimeSpan timeSpan) Compile(string code) {
+        var sw = Stopwatch.StartNew();
         Result<string> result = Compiler.Run(code, "/", new ConfigData());
+        sw.Stop();
         if (!result.TryGetValue(out var abap)) {
-            return new MarkupString($"<span class=\"error\">Error at line {result.Error.Line}: {result.Error.Text}</span>");
+            return (new MarkupString($"<span class=\"error\">Error at line {result.Error.Line}: {result.Error.Text}</span>"), TimeSpan.Zero);
         }
         
         
         var html = abap.Replace("\n", "<br>").Replace(" ", "&nbsp;");
-        return new MarkupString(html);
+        return (new MarkupString(html), sw.Elapsed);
     }
 }
